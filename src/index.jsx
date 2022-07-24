34c09e6bdad7c5bbe547949aab61d28d439cc25a
chineseJack564 <zyan@uc.cl>
2022-07-23 23:49:02 -0400

    ready to deploy

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import AppRouter from './routers/AppRouter';
 import "./App.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AppRouter />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
